f6e9e21a74129143eb16103d0466060e6ea253ff
Zack Ward <zackward.student@gmail.com>
2024-03-05 11:54:58 -0800

    made the breakpoint happen on lg
    fixed some awkward views

--- app/page.tsx
+++ app/page.tsx
@@ -7,30 +7,31 @@ import "./globals.css";
 export default function Home() {
   return (
     <main className="grid grid-cols-12 bg-black text-white min-h-screen">
-      <div className="order-2 md:order-1 grid grid-cols-12 col-start-1 md:col-end-3 col-end-11 bg-slate-900 rounded-full mr-20 border-r-4 border-slate-700">
-        <div className="bg-slate-900 flex-initial h-full col-span-5 -mr-16 pl-4">
+      <div className="order-2 lg:order-1 grid grid-cols-12 col-start-1 lg:col-end-4 col-end-11 bg-slate-900 rounded-full mr-20 border-r-4 border-slate-700">
+        <div className="bg-slate-900 flex-initial h-full lg:col-span-4 col-span-4 -mr-16 pl-4 py-2">
           <ListItems />
         </div>{" "}
       </div>
-      <div className="order-1 md:order-2 md:col-start-4 md:col-end-9 col-span-full justify-center">
-        <h1 className="text-center text-6xl font-bold text-blue-400 py-4">
+      <div className="order-1 lg:order-2 lg:col-start-4 lg:col-end-10 col-span-full justify-center">
+        <h1 className="text-center text-6xl font-bold text-blue-400 py-3">
           Zack Ward
         </h1>
         <Image
           className="mx-auto rounded-full transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
           src={profilePic}
+          priority
           width={300}
           height={300}
           alt="Picture of the author Zack Ward. He is a large man with a beard, a good smile, and a bright shirt."
         />
-        <div className="col-start-2 col-end-5 justify-center bg-slate-900 rounded-full border-x-4 border-slate-700 px-16 py-8 m-4">
+        <div className="col-start-2 col-end-5 justify-center bg-slate-900 rounded-full border-x-4 border-slate-700 px-12 py-9 m-5">
           <h2 className="text-center text-2xl mb-4 ">Software Engineer</h2>
           <p className="text-center mb-4">
             I have a Bachelors of Science in Computer Science from Eastern
             Washington University.
           </p>
           <p></p>
-          <p className="flex-shrink w-auto text-left pb-4">
+          <p className="flex-shrink w-auto text-left mb-12">
             I am eager to find a workplace that values continuous learning and
             development, both personally and professionally. During my six-month
             internship at{" "}
