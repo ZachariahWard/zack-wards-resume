9b2204dc537c9ef5aebe4934989a4a3b0988e29a
Zack Ward <zackward.student@gmail.com>
2024-03-05 13:05:34 -0800

    updated the blurb
    simplified the text to explain who I am.

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <div className="order-2 lg:order-1 grid grid-cols-12 col-start-1 lg:col-end-4 col-end-11 bg-slate-900 rounded-full mr-20 border-r-4 border-slate-700">
         <div className="bg-slate-900 flex-initial h-full lg:col-span-4 col-span-4 -mr-16 pl-4 py-2">
           <ListItems />
-        </div>{" "}
+        </div>
       </div>
       <div className="order-1 lg:order-2 lg:col-start-4 lg:col-end-10 col-span-full justify-center">
         <h1 className="text-center text-6xl font-bold text-blue-400 py-3">
@@ -26,26 +26,16 @@ export default function Home() {
         />
         <div className="col-start-2 col-end-5 justify-center bg-slate-900 rounded-full border-x-4 border-slate-700 px-12 py-9 m-5">
           <h2 className="text-center text-2xl mb-4 ">Software Engineer</h2>
-          <p className="text-center mb-4">
-            I have a Bachelors of Science in Computer Science from Eastern
-            Washington University.
+          <p className="flex-shrink w-auto text-justify mb-4">
+            I am a full-time Christian, Dad, and Problem Solver. I am also a
+            Software Engineer with experience in full-stack development, client
+            communication, web design, the fundamentals of machine learning, and
+            asking insightful questions.
           </p>
-          <p></p>
-          <p className="flex-shrink w-auto text-left mb-12">
-            I am eager to find a workplace that values continuous learning and
-            development, both personally and professionally. During my six-month
-            internship at{" "}
-            <Link
-              href="https://intellitect.com/"
-              className="font-semibold text-blue-400"
-            >
-              IntelliTect
-            </Link>{" "}
-            in Spokane, I had the opportunity to work with two teams in a
-            Scrum-based agile development environment. This experience taught me
-            the importance of effective communication and the value of seeking
-            help when needed. I am grateful for the skills I gained during my
-            internship and am excited to bring them to a new opportunity.
+          <p className="flex-shrink w-auto text-justify mb-12">
+            I am passionate about acquiring deep knowledge in full-stack
+            engineering, UI/UX design, machine learning, and software
+            architecture.
           </p>
         </div>
       </div>
